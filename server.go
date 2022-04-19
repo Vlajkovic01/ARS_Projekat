0ed834fb52e64b113dd05f4fa033ba71cdecd3c3
belom <belomarkovic1@gmail.com>
2022-04-19 16:44:03 +0200

    servers file init

---
+++ server.go
@@ -0,0 +1,13 @@
+package main
+
+type postServer struct {
+	data map[string]*Config
+}
+
+func (ts *postServer) createPostHandler() {}
+
+func (ts *postServer) getAllHandler() {}
+
+func (ts *postServer) getPostHandler() {}
+
+func (ts *postServer) delPostHandler() {}
