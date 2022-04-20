33f35eaf518d56c5bf0fa6399496c2ce7b9170e6
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2022-04-20 09:05:40 +0200

    Added initial delete handler

--- server.go
+++ server.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"errors"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
 type postServer struct {
-	data map[string]*Config
+	data map[string][]*Config
 }
 
 func (ts *postServer) createPostHandler() {}
@@ -10,4 +16,13 @@ func (ts *postServer) getAllHandler() {}
 
 func (ts *postServer) getPostHandler() {}
 
-func (ts *postServer) delPostHandler() {}
+func (ts *postServer) delPostHandler(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	if v, ok := ts.data[id]; ok {
+		delete(ts.data, id)
+		renderJSON(w, v)
+	} else {
+		err := errors.New("key not found")
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+}
